729a389b59a0fa9ff9fac0cbcc2ba2301f43f1df
Pravin Sarule <pravinsarule617@gmail.com>
2025-06-09 15:45:36 +0530

    mongodb

--- backend/models/detectionModel.js
+++ backend/models/detectionModel.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose');
 
 const detectionSchema = new mongoose.Schema({
-  userId: Number,   // from PostgreSQL
-  plantName: String,
-  diseaseName: String,
-  imageUrl: String,
+  userId: {
+    type: String, // or mongoose.Schema.Types.ObjectId if referencing a User model
+    required: true,
+  },
+  plantName: {
+    type: String,
+    required: true,
+  },
+  diseaseName: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: false,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
-  }
+  },
 });
 
 module.exports = mongoose.model('Detection', detectionSchema);
