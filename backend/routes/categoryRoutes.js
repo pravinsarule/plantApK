82cea28c05f37446f8515b851a6677859c83e078
vishalbainade <vishalbainade27@gmail.com>
2025-06-16 12:21:52 +0530

    Delete backend directory

--- backend/routes/categoryRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const categoriesController = require('../controllers/categoryController');
-
-router.get('/categories', categoriesController.getAllCategories);
-
-module.exports = router;
